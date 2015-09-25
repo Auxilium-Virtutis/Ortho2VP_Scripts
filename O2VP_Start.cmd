b8bd6db9d12ffae8edcea870e457de6267909ec4
Phill Fernandes <pfernandes@itsejin.com>
2015-09-25 17:37:15 -0400

    Adding scripts to repo.

---
+++ O2VP_Start.cmd
@@ -0,0 +1,7 @@
+rem For Ortho II Viewpoint
+rem
+@ECHO OFF
+net start mssqlserver
+net start callservice
+net start preprocessor
+
---
+++ O2VP_Stop.cmd
@@ -0,0 +1,7 @@
+rem For Ortho II Viewpoint
+rem
+@ECHO OFF
+net stop callservice
+net stop mssqlserver
+net stop preprocessor
+
